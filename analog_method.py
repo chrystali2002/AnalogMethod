1a108e8fe6a048194bf3b7fb93bcb8c0f7357cb4
Martin Schneider <martinschneider@Martins-MacBook-Air-2.local>
2019-06-20 11:14:59 +0200

    Boosted version of analog_method.py

--- analog_method.py
+++ analog_method.py
@@ -1,7 +1,7 @@
 import xarray as xr
 import numpy as np
 import dask
-from reanalysis import GCM_data
+#from reanalysis import GCM_data
 #from eofs.iris import MultivariateEof
 from eofs.iris import Eof
 #from eofs.xarray import Eof
@@ -31,8 +31,8 @@ data_ano = xr.open_mfdataset(path+'anom_all_1979-2018.nc', chunks={'time': 5000}
 
 #%%
 # Create time series for which the analoga should be calculated.
-time_start = data_ano.time.to_series().dt.strftime('%Y-%m-%d')[0]
-time_end = data_ano.time.to_series().dt.strftime('%Y-%m-%d')[3]
+time_start = '1970-01-01'
+time_end = '1970-12-31'
 
 #time_end = data_ano.time.to_series().dt.strftime('%Y-%m-%d')[-1]
 timeseries = pd.date_range(time_start, time_end, freq='D')
@@ -40,12 +40,15 @@ timeseries = pd.date_range(time_start, time_end, freq='D')
 # Define number of analogons to be found for each day in timeseries.
 no_analoga = 5
 
+# Define number of years in dataset.
+no_years = len(data_ano.groupby('time.year'))
+
 # Create empty array to be filled with 1st,2nd,3rd,... analoga.
 # First column represents days for which analoga should be found.
 # Second column = 1st analogon.
 # Third column = 2nd analogon. etc.
-analoga = np.zeros((len(timeseries), no_analoga+1), dtype='datetime64[s]')
-analoga[:,0] = timeseries
+analoga = np.zeros((len(data_ano.time), no_analoga+1), dtype='datetime64[s]')
+analoga[:,0] = data_ano.time
 #%%
 
 # Run time of script is observed with t_start and t_end respectively.
@@ -62,22 +65,13 @@ for i, day in enumerate(timeseries):
     
     # Select all doy of query_period.
     query_doy = query_period.dayofyear
-    
-    # Delete days before and after time range of entire dataset.
-    # Relevant for first and last days of dataset.
-    # e.g. (1978-12-22, 2019-01-10, etc.)
-    query_period = query_period[query_period >= data_ano.time.to_series()[0]]
-    query_period = query_period[query_period <= data_ano.time.to_series()[-1]]
 
     # Define dataset to work with.
     # Select all doy in dataset to use for pc (and eof) calculation.
     ds = data_ano.sel(time = data_ano.time.dt.dayofyear.isin(query_doy))
-    
-    # Delete current year from dataset.
-    ds = ds.sel(time = ~ds.time.dt.year.isin(day.year))
 
-    # Define dataset for current day (required for ppc calculation)
-    ds_day = data_ano.sel(time=day)
+    # Define dataset for current doy (required for ppc calculation)
+    ds_doy = data_ano.sel(time = data_ano.time.dt.dayofyear.isin(day.dayofyear))
 
     # Assign attrs 'T' to time dimension (required for EOF analysis package)
     ds.coords['time'].attrs['axis'] = 'T'
@@ -107,38 +101,48 @@ for i, day in enumerate(timeseries):
     # Transform iris cube of pcs to DataArray (xarray)
     da_pc = xr.DataArray.from_iris(pc).rename('pc')
     
-    # Convert DataArrays of current day to iris cubes.
-    ds_iris_r_query_day = ds_day.r.to_iris()
-    ds_iris_q_query_day = ds_day.q.to_iris()
-    ds_iris_msl_query_day = ds_day.msl.to_iris()
+    # Convert DataArrays of current doy to iris cubes.
+    # ppc calculation is vectorized and therefore carried out simultaneously
+    # for each doy.
+    ds_iris_r_query_doy = ds_doy.r.to_iris()
+    ds_iris_q_query_doy = ds_doy.q.to_iris()
+    ds_iris_msl_query_doy = ds_doy.msl.to_iris()
 
     # Merge iris cubes to list of iris cubes.
-    cube_list_query_day = iris.cube.CubeList([ds_iris_r_query_day, \
-                                    ds_iris_q_query_day, ds_iris_msl_query_day])
-    ds_iris_query_day = cube_list_query_day.merge()     
-    
+    cube_list_query_doy = iris.cube.CubeList([ds_iris_r_query_doy, \
+                                    ds_iris_q_query_doy, ds_iris_msl_query_doy])
+    ds_iris_query_doy = cube_list_query_doy.merge()     
+
     # Calculate ppcs (Pseudo-PCs)
-    pseudo_pc = solver.projectField(ds_iris_query_day, neofs=n)
-    
+    pseudo_pc = solver.projectField(ds_iris_query_doy, neofs=n)
+
     # Transform iris cube of pseudo-pcs to DataArray (xarray)
     da_pseudo_pc = xr.DataArray.from_iris(pseudo_pc).rename('pseudo_pc')
-    
-    # Caclulate norm.
-    norm = (( da_pc - da_pseudo_pc )**2).sum(dim='pc')
-    
-    print('Looking for analoga for: ', day)
-    for j in range(1,no_analoga+1):
-        # Find analogon (day in dataset) through localization of minimum norm.
-        minimum_idx = np.argmin(norm).values
-        analogon = ds.time.to_series()[minimum_idx]
-        
-        # Add analogon to analogon array
-        analoga[i][j] = analogon
-        
-        # Set minimum value to nan in norm array to find 2nd, 3rd, ... minimum.
-        norm = norm.where(norm.values != norm[minimum_idx].values)
 
-        print(str(j) +  '.analogon: ', str(analogon)[:10])
+    for y in range(0, no_years):
+        # Caclulate norm.
+        norm = (( da_pc - da_pseudo_pc[y] )**2).sum(dim='pc')
+        # Delete current year y from norm array.
+        norm = norm.sel(time = ~norm.time.dt.year.isin(da_pseudo_pc.time.to_series()[y].year))
+        str_curr_day = da_pseudo_pc.time.to_series().dt.strftime('%Y-%m-%d')[y]
+
+        print('Looking for analoga for: ', str_curr_day)
+        for a in range(1,no_analoga+1):
+            # Find analogon (day in dataset) through localization of minimum norm.
+            minimum_idx = np.argmin(norm).values
+            # Temporarily delete current year y from ds.
+            ds_tmp = ds.sel(time = ~ds.time.dt.year.isin(da_pseudo_pc.time.to_series()[y].year))
+            analogon = ds_tmp.time.to_series()[minimum_idx]
+            
+            # Add analogon to analogon array
+            # row... select row of current day in analoga array.
+            row = i + y*no_years
+            analoga[row][a] = analogon
+            
+            # Set minimum value to nan in norm array to find 2nd, 3rd, ... minimum.
+            norm = norm.where(norm.values != norm[minimum_idx].values)
+    
+            print(str(a) +  '.analogon: ', str(analogon)[:10])
     
     t_end_tmp = time.time()
     print("total time: ", str(datetime.timedelta(seconds=t_end_tmp - t_start)), '\n')
